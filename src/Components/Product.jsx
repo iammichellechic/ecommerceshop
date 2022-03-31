f280c7ef0a1c3e962ca81b6e8e587a4b43ac532f
iammichellechic <michellechicsarmiento@yahoo.com>
2022-03-31 16:38:57 +0200

    v2

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -3,9 +3,12 @@ import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import { addItem } from "../redux/action";
+import Skeleton from "react-loading-skeleton";
+import {Ratings} from "./Ratings";
 
 export const Product = () => {
   const [product, setSingleItem] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   let params = useParams();
   let { id } = params;
@@ -17,46 +20,76 @@ export const Product = () => {
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       const response = await fetch(`https://fakestoreapi.com/products/${id}`);
       setSingleItem(await response.json());
+      setLoading(false);
     };
     fetchData();
   }, []);
+
+  const Loading = () => {
+    return (
+      <>
+        <div className="col-md-6">
+          <Skeleton height={400} />
+        </div>
+        <div className="col-md-6" style={{ lineHeight: 2 }}>
+          <Skeleton height={50} width={300} />
+          <Skeleton height={75} />
+          <Skeleton height={25} width={150} />
+          <Skeleton height={50} />
+          <Skeleton height={150} />
+          <Skeleton height={50} width={100} />
+          <Skeleton height={50} width={100} style={{ marginLeft: 6 }} />
+        </div>
+      </>
+    );
+  };
+
+  const ShowProduct = () => {
+    return (
+      <>
+        <div className="col-md-6">
+          <img
+            src={product.image}
+            alt={product.title}
+            height="500px"
+            width="400px"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="text-uppercase text-black-50">{product.category}</h4>
+          <h1 className="display-5">{product.title}</h1>
+          <p className="lead fw-bolder">
+            Rating {product.rating && product.rating.rate}
+            <i className="fa fa-star"></i>
+          </p>
+          <Ratings rating={product.rating && product.rating.rate} />
+          <h3 className="display-6 fw-bold my-4">
+            {" "}
+            Price: {product.price} SEK{" "}
+          </h3>
+          <p className="lead">{product.description}</p>
+          <button
+            className="btn btn-outline-dark px-4 py-2"
+            onClick={() => addProduct(product)}
+          >
+            Add to Cart
+          </button>
+          <NavLink to="/cart" className="btn btn-dark ms-2 px-3 py-2">
+            Go to Cart
+          </NavLink>
+        </div>
+      </>
+    );
+  };
 
   return (
     <div>
       <div className="container py-5">
         <div className="row py-4">
-          <div className="col-md-6">
-            <img
-              src={product.image}
-              alt={product.title}
-              height="500px"
-              width="400px"
-            />
-          </div>
-          <div className="col-md-6">
-            <h4 className="text-uppercase text-black-50">{product.category}</h4>
-            <h1 className="display-5">{product.title}</h1>
-            <p className="lead fw-bolder">
-              Rating {product.rating && product.rating.rate}
-              <i className="fa fa-star"></i>
-            </p>
-            <h3 className="display-6 fw-bold my-4">
-              {" "}
-              Price: {product.price} SEK{" "}
-            </h3>
-            <p className="lead">{product.description}</p>
-            <button
-              className="btn btn-outline-dark px-4 py-2"
-              onClick={() => addProduct(product)}
-            >
-              Add to Cart
-            </button>
-            <NavLink to="/cart" className="btn btn-dark ms-2 px-3 py-2">
-              Go to Cart
-            </NavLink>
-          </div>
+          {loading ? <Loading /> : <ShowProduct />}
         </div>
       </div>
     </div>
